d344b8391f4d9a4ce2f7a526189ebef34ef2edef
nhatminhdev <heomeco2014@gmail.com>
2023-05-25 03:07:33 +0700

    feat: upload image from local

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -30,27 +30,25 @@ function PlacesPage() {
         })
         setAddedPhotos([...addedPhotos, data])
     }
+
+    function uploadPhoto(ev) {
+        const file = ev.target.files
+        const data = new FormData()
+        for (let i = 0; i < file.length; i++) {
+            data.append('file', file[i])
+        }
+        axios.post('/upload-local', data, { headers: { 'Content-Type': 'multipart/form-data' } }).then((response) => {
+            const { data: filename } = response
+            setAddedPhotos([...addedPhotos, ...filename])
+        })
+    }
     return (
         <div>
             {action !== 'new' && (
                 <div className="text-center">
-                    <Link
-                        className="mx-auto inline-flex items-center gap-1 rounded-full bg-primary px-4 py-2 text-white"
-                        to={'/account/places/new'}
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="h-6 w-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 4.5v15m7.5-7.5h-15"
-                            />
+                    <Link className="mx-auto inline-flex items-center gap-1 rounded-full bg-primary px-4 py-2 text-white" to={'/account/places/new'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                         Add new places
                     </Link>
@@ -59,40 +57,15 @@ function PlacesPage() {
             {action === 'new' && (
                 <div className="">
                     <form>
-                        {preInput(
-                            'Title',
-                            'Title your place, should be short and catchy as in adsvertisement'
-                        )}
-                        <input
-                            type="text"
-                            placeholder="title, example: My home"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                        />
-                        {preInput(
-                            'Address',
-                            'Address of your place, should be real address'
-                        )}
-                        <input
-                            type="text"
-                            placeholder="address"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                        />
+                        {preInput('Title', 'Title your place, should be short and catchy as in adsvertisement')}
+                        <input type="text" placeholder="title, example: My home" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        {preInput('Address', 'Address of your place, should be real address')}
+                        <input type="text" placeholder="address" value={address} onChange={(e) => setAddress(e.target.value)} />
 
                         {preInput('Photos', 'More = better')}
                         <div className="flex items-center gap-2">
-                            <input
-                                type="text"
-                                placeholder="Add using a link"
-                                className=""
-                                value={photoLinks}
-                                onChange={(e) => setPhotoLinks(e.target.value)}
-                            />
-                            <button
-                                className="w-[7%] rounded-2xl bg-gray-300 px-4 py-2 text-black"
-                                onClick={addPhotoByLink}
-                            >
+                            <input type="text" placeholder="Add using a link" className="" value={photoLinks} onChange={(e) => setPhotoLinks(e.target.value)} />
+                            <button className="w-[7%] rounded-2xl bg-gray-300 px-4 py-2 text-black" onClick={addPhotoByLink}>
                                 Add photo
                             </button>
                         </div>
@@ -100,98 +73,45 @@ function PlacesPage() {
                             {addedPhotos.length > 0 &&
                                 addedPhotos.map((photo) => {
                                     return (
-                                        <img
-                                            className="rounded-2xl "
-                                            src={
-                                                'http://localhost:4000/uploads/' +
-                                                photo
-                                            }
-                                        />
+                                        <div className="flex h-32">
+                                            <img className="w-full rounded-2xl object-cover" src={'http://localhost:4000/uploads/' + photo} />
+                                        </div>
                                     )
                                 })}
-                            <button className="inline-flex items-center justify-center gap-3 rounded-2xl border bg-transparent p-8 text-2xl">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="h-6 w-6"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                                    />
+                            <label className="inline-flex items-center justify-center gap-3 rounded-2xl border bg-transparent p-8 text-2xl">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                                 </svg>
+                                <input type="file" className="hidden" onChange={uploadPhoto} multiple />
                                 Upload
-                            </button>
+                            </label>
                         </div>
                         {preInput('Description', 'Description of the place')}
-                        <textarea
-                            className=""
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        ></textarea>
+                        <textarea className="" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
 
-                        {preInput(
-                            'Perks',
-                            ' Select all the perks that your place has'
-                        )}
+                        {preInput('Perks', ' Select all the perks that your place has')}
 
-                        <Perks
-                            selected={perks}
-                            onChange={setPerks}
-                        />
+                        <Perks selected={perks} onChange={setPerks} />
 
                         {preInput('Extra info', 'House rules, etc')}
-                        <textarea
-                            className=""
-                            value={extraInfor}
-                            onChange={(e) => setExtraInfor(e.target.value)}
-                        ></textarea>
-                        {preInput(
-                            'Check in & out times',
-                            'Add check in and out times, remember to have some time window cleaning the room between guests'
-                        )}
+                        <textarea className="" value={extraInfor} onChange={(e) => setExtraInfor(e.target.value)}></textarea>
+                        {preInput('Check in & out times', 'Add check in and out times, remember to have some time window cleaning the room between guests')}
 
                         <div className="grid gap-2 sm:grid-cols-3">
                             <div className="">
                                 <h3 className="mt-2">Check in times</h3>
-                                <input
-                                    type="time"
-                                    className=""
-                                    placeholder="13:00"
-                                    value={checkIn}
-                                    onChange={(e) => setCheckIn(e.target.value)}
-                                />
+                                <input type="time" className="" placeholder="13:00" value={checkIn} onChange={(e) => setCheckIn(e.target.value)} />
                             </div>
                             <div className="">
                                 <h3 className="mt-2">Check out times</h3>
-                                <input
-                                    type="time"
-                                    value={checkOut}
-                                    onChange={(e) =>
-                                        setCheckOut(e.target.value)
-                                    }
-                                />
+                                <input type="time" value={checkOut} onChange={(e) => setCheckOut(e.target.value)} />
                             </div>
                             <div className="">
                                 <h3 className="mt-2">Max number of guest</h3>
-                                <input
-                                    type="number"
-                                    min={0}
-                                    max={100}
-                                    value={maxGuests}
-                                    onChange={(e) =>
-                                        setMaxGuests(e.target.value)
-                                    }
-                                />
+                                <input type="number" min={0} max={100} value={maxGuests} onChange={(e) => setMaxGuests(e.target.value)} />
                             </div>
                         </div>
-                        <button className="primary mx-auto mt-2 block max-w-2xl">
-                            Save
-                        </button>
+                        <button className="primary mx-auto mt-2 block max-w-2xl">Save</button>
                     </form>
                 </div>
             )}
