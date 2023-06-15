a1214f31989ef8f001468d071bed402c147f0084
nhatminhdev <heomeco2014@gmail.com>
2023-06-15 17:28:54 +0700

    feat: cancel booking

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, redirect, useParams } from 'react-router-dom'
 import LinkAddress from '../components/LinkAddress'
 import Gallery from '../components/Gallery'
 import BookingDate from '../components/BookingDate'
-
+import { IconTrash } from '@tabler/icons-react'
+import { Navigate } from 'react-router-dom'
 function BookingPage() {
-    const { id } = useParams()
+    let { id } = useParams()
     const [booking, setBooking] = useState(null)
+    const [redirect, setRedirect] = useState(false)
     useEffect(() => {
         if (id) {
             axios.get(`/bookings`).then((res) => {
@@ -15,9 +17,17 @@ function BookingPage() {
                 if (foundBooking) setBooking(foundBooking)
             })
         }
-    }, [])
-
-    if (!booking) return 'Loading...'
+    }, [id])
+    useEffect(() => {}, [booking])
+    async function handleDelete(ev) {
+        ev.preventDefault()
+        console.log('delete')
+        await axios.delete(`/account/bookings/${id}`)
+        setBooking(null)
+        setRedirect(true)
+    }
+    if (!booking && !redirect) return 'Loading...'
+    if (redirect) return <Navigate to={'/account/bookings'} />
     return (
         <div className="">
             <h1 className="text-3xl">{booking.place.title}</h1>
@@ -25,9 +35,17 @@ function BookingPage() {
                 place={booking.place.address}
                 className="my-2"
             />
+
             <div className="bg-gray-200 px-4 py-2 rounded-2xl flex flex-col">
                 <div className="text-2xl translate-y-6 -my-4">Your booking information</div>
                 <BookingDate booking={booking} />
+                <button
+                    className="btn flex self-start items-center gap-x-1 text-xl font-thin hover:text-white "
+                    onClick={(ev) => handleDelete(ev)}
+                >
+                    <IconTrash />
+                    Delete this booking
+                </button>
             </div>
             <Gallery place={booking.place} />
         </div>
